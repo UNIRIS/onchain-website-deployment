0a9325589ec63bd87be635a6c5dc4fd9a066e9d0
Samuel Manzanera <samuel@uniris.io>
2020-07-03 18:07:21 +0200

    Add page for asset deployment using the SDK

---
+++ app.js
@@ -0,0 +1,42 @@
+import { derivateKeyPair, newTransactionBuilder, sendTransaction } from 'uniris';
+let content = ""
+
+const fileSelector = document.querySelector('#file');
+  fileSelector.addEventListener('change', (event) => {
+    const fileList = event.target.files;
+
+    let fr = new FileReader()
+    fr.onload = function (e) {
+        content = e.target.result
+    }
+    fr.readAsText(fileList[0])
+  });
+
+window.generateHostingTransaction = function() {
+
+    const originSeed = document.querySelector("#originSeed").value
+    const { privateKey: originPrivateKey } = derivateKeyPair(originSeed, 0)
+
+    const seed = document.querySelector("#seed").value
+    const index = parseInt(document.querySelector("#index").value)
+
+    const tx = newTransactionBuilder("hosting")
+    .setContent(content)
+    .build(seed, index, originPrivateKey)
+
+    document.querySelector("#tx_json").innerText = JSON.stringify(tx, undefined, 2)
+    document.querySelector("#output").style.display = "block"
+}
+
+window.sendTransaction = function() {
+    const txJSON = document.querySelector("#tx_json").innerText
+    sendTransaction(JSON.parse(txJSON), "http://localhost:4000")
+        .then(data => {
+            if (data.errors) {
+                alert("Something went wrong: " + JSON.stringify(data.errors))
+                return
+            }
+
+            alert("Transaction submited to the network")
+        })
+}
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+module.exports = {
+    entry: './app.js',
+    output: {
+      filename: 'app.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: "Uniris",
+      libraryTarget: "umd"
+    },
+    devtool: 'source-map'
+}
