009896f371f217376211f9721ef2aca078ebb158
Samuel Manzanera <samuel@uniris.io>
2020-11-04 16:31:25 +0100

    improve UI and update SDK dependency

--- app.js
+++ app.js
@@ -1,58 +1,30 @@
-import { derivateKeyPair, newTransactionBuilder, sendTransaction, derivateAddress, getTransactionIndex } from 'uniris';
-let content = ""
-let tx
+import { deriveAddress, deriveKeyPair, newTransactionBuilder, sendTransaction, getTransactionIndex } from 'uniris';
 
-const fileSelector = document.querySelector('#file');
-  fileSelector.addEventListener('change', (event) => {
-    const fileList = event.target.files;
-
-    let fr = new FileReader()
-    fr.onload = function (e) {
-        content = e.target.result
-    }
-    fr.readAsArrayBuffer(fileList[0])
-  });
-
-window.generateHostingTransaction = function() {
-
-    const originSeed = document.querySelector("#originSeed").value
-    const { privateKey: originPrivateKey } = derivateKeyPair(originSeed, 0)
-
-    const seed = document.querySelector("#seed").value
-    const index = parseInt(document.querySelector("#index").value)
-
-    tx = newTransactionBuilder("hosting")
+window.generateTransaction = (seed, index, content) => {
+    const { privateKey: originPrivateKey } = deriveKeyPair(originSeed, 0)
+    return newTransactionBuilder("hosting")
         .setContent(content)
         .build(seed, index)
         .originSign(originPrivateKey)
-
-    document.querySelector("#tx_json").innerText = tx.address.toString('hex')
-    document.querySelector("#output").style.display = "block"
 }
 
-window.sendTransaction = function() {
-    const endpoint = document.querySelector("#endpoint").value
-    document.querySelector("#process").style.display = "block"
-    sendTransaction(tx, endpoint)
-        .then(data => {
-            document.querySelector("#process").style.display = "none"
+window.sendTransaction = async (tx, endpoint) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const data = await sendTransaction(tx, endpoint)
             if (data.errors) {
-                alert("Something went wrong: " + JSON.stringify(data.errors))
-                return
+                return reject(JSON.stringify(data.errors))
             }
-
-            alert("Transaction submited to the network")
-        })
-        .catch(() => {
-            document.querySelector("#process").style.display = "block"
-        })
+            return resolve()
+        }
+        catch (e) {
+            return reject(e)
+        }
+    })
 }
 
-window.getTransactionIndex = function() {
-    const endpoint = document.querySelector("#endpoint").value
-    const seed = document.querySelector("#seed").value
-    const address = derivateAddress(seed, 0)
-    getTransactionIndex(address, endpoint).then((nb) => {
-        document.querySelector("#index").value = nb
-    })
+window.getTransactionIndex = async (seed, endpoint) => {
+    const address = deriveAddress(seed, 0);
+    const nb = await getTransactionIndex(address, endpoint)
+    return nb
 }
